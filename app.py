2bb107b7f751455dc1ddc106fb486c90dc61c09a
Prideland-Okoi <prideland.okoi@gmail.com>
2024-10-09 20:46:40 +0100

    Update app.py
    moved db creation fron config to app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from api import create_app
+from api import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     # Run the application using the Flask development server
     app.run(host='0.0.0.0', port=5000, debug=True)
